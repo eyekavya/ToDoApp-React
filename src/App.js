82b2ec52f3ec859a793e49134af69dcee510be65
eyekavya <kavyasrivastava7830@gmail.com>
2023-03-22 13:08:39 +0530

    added basic styling and header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 
 import React from "react";
+import Heading from "./components/Heading/Heading";
 
 function App() {
-  return <div>To Do</div>;
+  return (
+    <>
+      <Heading />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <>
+      <header>
+        <h1>Kavya's TO-DO List</h1>
+      </header>
+    </>
+  );
+}
+
+export default Heading;
